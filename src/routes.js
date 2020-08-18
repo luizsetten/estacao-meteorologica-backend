a7250cae026cc1fa80affa53a5122462b1f159fd
Luiz Gustavo C Setten <luizgustavosetten@hotmail.com>
2020-08-18 11:10:10 -0300

    feat: Insomnia workspace added

--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,11 @@ router.post('/stations', StationController.store);
 router.put('/stations/:station_id', StationController.update);
 router.delete('/stations/:station_id', StationController.delete);
  
+//Inserir middleware de validação de dados
+
 //Logs routes
 router.get('/stations/:station_id/logs', LogController.index);
 router.post('/stations/:station_id/logs', LogController.store);
-
-// router.delete('/stations/:station_id', LogController.delete);
+// router.update('/stations/:station_id', LogController.update); Metodo não implementado - sem necessidade
+// router.delete('/stations/:station_id', LogController.delete); Metodo não implementado - sem necessidade
 module.exports = router;
