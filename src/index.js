6095ff7c6f0c35f1251e7de5a8513deb75b6bd88
Luiz Gustavo C Setten <luizgustavosetten@hotmail.com>
2020-09-26 08:34:21 -0300

    feat: Cors added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes.js');
 
@@ -6,7 +7,9 @@ require('./database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 
 app.use(routes);
 
